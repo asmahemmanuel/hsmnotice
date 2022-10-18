2c904b525f2e258153bd400cce0ab35f3c42c0c4
asmahemmanuel <atbhae100@example.com>
2022-10-18 09:09:47 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>HSM PROGRAM OUTLINE</h1><hr /><br />
+            <h2>CONFERENCE: Realities of the Spirit Conference. 22nd October, 12PM @ Yiri Lodge</h2>
+            <h2>DESTINY PRAYERS: Tuesdays @ 7PM. Online Service</h2>
+            <h2>SCHOOL OF THE SPIRIT: Thursdays @ 7PM. Online Service</h2>
+        </div>
+    )
+}
+
+export default Header;
